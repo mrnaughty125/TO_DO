a86388f77026636ee03c22b5dee91ec02156e9a4
karri pradeep kumar <20A91A1221@aec.edu.in>
2023-12-28 16:48:16 +0530

    Update addtask.js
    add the green tick mark

--- addtask.js
+++ addtask.js
@@ -4,28 +4,29 @@ document.addEventListener('DOMContentLoaded', () => {
     if (savedTasks) {
         document.getElementById('task').innerHTML = savedTasks;
 
-        // Reattach delete function to delete icons
-        attachDeleteFunction();
+        // Reattach delete and check functions to icons
+        attachIconFunctions();
     }
 });
 
 let add = document.getElementById('addbtn');
 add.addEventListener('click', () => {
     let p = document.getElementById('inp').value;
-    document.getElementById('inp').value="";
+    document.getElementById('inp').value = "";
     if (p == "") {
         alert("Add a Valid task");
     } else {
         let ele = document.createElement('li');
         let div = document.createElement('div');
         let icon1 = document.createElement("i");
-        // let icon2 = document.createElement("i");
+        let icon2 = document.createElement("i");
         ele.textContent = p;
         icon1.style.color = "red";
-        // icon2.style.color = "green";
+        icon2.style.color = "green";
         icon1.classList.add("fa", "fa-times");
-        // icon2.classList.add("fa", "fa-check");
+        icon2.classList.add("fa", "fa-check");
         div.appendChild(icon1);
+        div.appendChild(icon2);
         ele.appendChild(div);
         document.getElementById('task').appendChild(ele);
         ele.classList.add("alltasks");
@@ -33,11 +34,22 @@ add.addEventListener('click', () => {
         icon1.addEventListener('click', () => {
             document.getElementById('task').removeChild(ele);
             saveTasksToLocalStorage(); // Save updated tasks to local storage
-            attachDeleteFunction(); // Reattach delete function to delete icons
+            attachIconFunctions(); // Reattach delete and check functions to icons
         });
 
+        // icon2.addEventListener('click', () => {
+        //     // Your logic for completing a task (if needed)
+        // });
+
+        icon2.addEventListener('click', () => {
+            // Your logic for completing a task (if needed)
+            ele.classList.add('completed-task'); // Add a class to change the style
+            saveTasksToLocalStorage(); // Save updated tasks to local storage
+        });
+        
+
         saveTasksToLocalStorage(); // Save new task to local storage
-        attachDeleteFunction(); // Attach delete function to delete icon
+        attachIconFunctions(); // Attach delete and check functions to icons
     }
 });
 
@@ -52,14 +64,20 @@ function saveTasksToLocalStorage() {
     localStorage.setItem('tasks', tasks);
 }
 
-function attachDeleteFunction() {
-    // Attach delete function to delete icons
+function attachIconFunctions() {
+    // Attach delete and check functions to icons
     document.querySelectorAll('.fa-times').forEach(icon => {
         icon.addEventListener('click', () => {
             const ele = icon.closest('li');
             document.getElementById('task').removeChild(ele);
             saveTasksToLocalStorage(); // Save updated tasks to local storage
-            attachDeleteFunction(); // Reattach delete function to delete icons
+            attachIconFunctions(); // Reattach delete and check functions to icons
+        });
+    });
+
+    document.querySelectorAll('.fa-check').forEach(icon => {
+        icon.addEventListener('click', () => {
+            // Your logic for completing a task (if needed)
         });
     });
 }
